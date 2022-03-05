e1509b0d742345a7a3bd9bd84af1ec73e7c52830
Adam Seitz <aseitz6@student.cscc.edu>
2022-03-05 18:27:27 -0500

    Added toString method to VideoGame class. Added developer attribute. Removed store attribute. In main, parses VideoGameList into VideoGame objects

---
+++ Main.java
@@ -0,0 +1,63 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		ArrayList<VideoGame> games = new ArrayList<>();
+		parseVideoGames(games);
+		//Test
+		for(VideoGame v:games) {
+			System.out.println(v.toString());
+		}
+		
+	}
+	/**
+	 * Parses text from VideoGamesList.txt to create VideoGameObjects
+	 * @param games type:ArrayList of VideoGames
+	 */
+	public static void parseVideoGames(ArrayList<VideoGame> games) {
+		File file;
+		Scanner input = null;
+		String inputLine;
+		String[] properties = new String[5]; 
+		String name;
+		String description;
+		String[] platforms;
+		String developer;
+		Double price;
+		
+		
+		try {
+			file = new File("VideoGameList.txt");
+			input = new Scanner(file);
+			while(input.hasNext()) {
+				inputLine = input.nextLine();
+				properties = inputLine.split("\t");
+				name = properties[0];
+				description = properties[1];
+				platforms = properties[2].split("/");
+				developer = properties[3];
+				price = Double.parseDouble(properties[4]);
+				games.add(new VideoGame(name,description,platforms, developer, price));
+			}
+			
+		} catch (FileNotFoundException e){
+			e.printStackTrace();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			input.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	
+	}
+
+}
--- VideoGame.java
+++ VideoGame.java
@@ -1,20 +1,33 @@
 public class VideoGame {
 	//=========================== Properties
-	private String name, store;
+	private String name, developer, description;
 	private String[] platforms;
 	private double price;
 	
 	//=========================== Constructors
 	public VideoGame() {
-	
+		
 	}
 	
-	public VideoGame(String name, String[] platforms, String store, double price) {
+	public VideoGame(String name, String description ,String[] platforms, String developer, double price) {
 		this.name = name;
-		this.store = store;
+		this.description = description;
+		this.developer = developer;
 		this.platforms = platforms;
 		this.price = price;
 	}
-	
+	public String toString() {
+		String platformsFormatted = "";
+		for(int i = 0; i < platforms.length; i++) {
+			if(i == platforms.length-1) {
+				platformsFormatted += platforms[i];
+			}
+			else {
+				platformsFormatted += platforms[i] + "/";
+			}
+		}
+		return "Name: " + name + " Description: " + description + " Platforms: " + platformsFormatted + " Developer: " + 
+				developer + " Price: " + price;
+	}
 	//=========================== Methods
 }
