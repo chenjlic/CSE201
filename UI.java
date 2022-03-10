026b81bbbc35556769561490ccce26208b2a255d
Adam Seitz <aseitz6@student.cscc.edu>
2022-03-09 20:48:49 -0500

    Collection of UI Componenets

--- UI.java
+++ UI.java
@@ -1,31 +1,22 @@
+import java.awt.BorderLayout;
+import java.util.ArrayList;
+
 import javax.swing.*;
 
 
-public class ui {
+public class UI {
 	
-	private static JFrame fr = new JFrame();
-	private static JButton button = new JButton("Click me");
-	
-	private static void simpleFrameWithButton() {
-		//setting up button
-		button.setBounds(150,150,100,50);		 
-		
-		//setting up frame
-		fr.setSize(400,400);
-		fr.setTitle("Video Game Catalog");
-		fr.setLayout(null);	
-		
-		//add button to frame
-		fr.add(button); 
-		
-		//make frame visible
-		fr.setVisible(true);
-
-	}
-
-	public static void main(String[] args) {
-	
-	simpleFrameWithButton();
+	public UI() {
+		ArrayList<VideoGame> games = new ArrayList<>();
+		Main.parseVideoGames(games);
+		JFrame jf = new JFrame("Video Game Catalog");
+		jf.setLayout(new BorderLayout());
+		jf.setSize(1400,600);
+		jf.setVisible(true);
+		jf.setResizable(false);
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ViewGames.viewGames(jf, games);
+		CreateAccount.createAccountButton(jf);
 		
 	}
 
