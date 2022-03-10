2a2c07fb8178cff1afccd6adf3c63b6c95c4e3e6
Adam Seitz <aseitz6@student.cscc.edu>
2022-03-09 20:50:43 -0500

    Added getters and setters

--- VideoGame.java
+++ VideoGame.java
@@ -16,6 +16,8 @@ public class VideoGame {
 		this.platforms = platforms;
 		this.price = price;
 	}
+	
+	//=========================== Methods
 	public String toString() {
 		String platformsFormatted = "";
 		for(int i = 0; i < platforms.length; i++) {
@@ -29,5 +31,45 @@ public class VideoGame {
 		return "Name: " + name + " Description: " + description + " Platforms: " + platformsFormatted + " Developer: " + 
 				developer + " Price: " + price;
 	}
-	//=========================== Methods
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDeveloper() {
+		return developer;
+	}
+
+	public void setDeveloper(String developer) {
+		this.developer = developer;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String[] getPlatforms() {
+		return platforms;
+	}
+
+	public void setPlatforms(String[] platforms) {
+		this.platforms = platforms;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
 }
