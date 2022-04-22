b3c5f1f088d06386beda0d62fc18f01f10cc1441
Adam Seitz <aseitz6@student.cscc.edu>
2022-04-22 00:03:42 -0400

    Fixed sort and table shows all entries correctly

--- ViewGames.java
+++ ViewGames.java
@@ -16,52 +16,56 @@ import javax.swing.table.TableColumnModel;
 
 
 public class ViewGames {
-	private static Object[] columnNames;
-	private static Object[][]data;
+	static Object[] columnNames;
+	static Object[][]data;
+	static JScrollPane scrollPane;
 	JPanel centerPanel;
 	static JTable table;
+	static DefaultTableModel model;
 	/**
 	 * @wbp.parser.entryPoint
 	 */
 	public static void viewGames(JFrame jf) {
 		ArrayList<VideoGame> games = new ArrayList<>();
 		Main.parseVideoGames(games);
-		columnNames = new Object[games.size()];
-		data = new Object[4][games.size()];
+		columnNames = new Object[games.size() + 1];
+		data = new Object[4][games.size() + 1];
 		columnNames[0] = "Title";
 		data[0][0] = "Description";
 		data[1][0] = "Platform";
 		data[2][0] = "Developer";
 		data[3][0] = "Price";
-		for (int i = 1; i < games.size() ; i++) {
-			columnNames[i] = games.get(i).getName();
+		for (int i = 0, k = 1; i < games.size() ; i++, k++) {
+			columnNames[k] = games.get(i).getName();
 		}
-		for (int i = 1; i < games.size() ; i++) {
-			data[0][i] = games.get(i).getDescription();
-			data[1][i] = games.get(i).getPlatformString();
-			data[2][i]= games.get(i).getDeveloper();
-			data[3][i] = games.get(i).getPrice();
+		for (int i = 0, k = 1; i < games.size() ; i++, k++) {
+			data[0][k] = games.get(i).getDescription();
+			data[1][k] = games.get(i).getPlatformString();
+			data[2][k]= games.get(i).getDeveloper();
+			data[3][k] = games.get(i).getPrice();
 		}
-		DefaultTableModel model = new DefaultTableModel(data, columnNames);
+		model = new DefaultTableModel(data, columnNames);
 		table = new JTable(model);
-		JScrollPane scrollPane = new JScrollPane(table);
-		TableColumn column1 = table.getColumnModel().getColumn(0);
-		//TableColumn column2 = table.getColumnModel().getColumn(1);
-		//TableColumn column3 = table.getColumnModel().getColumn(2);
-		//TableColumn column4 = table.getColumnModel().getColumn(3);
-	    //TableColumn column5 = table.getColumnModel().getColumn(4);
-		DefaultTableCellRenderer center = new DefaultTableCellRenderer();
-		center.setHorizontalAlignment( JLabel.CENTER );
+		scrollPane = new JScrollPane(table);
+	
+		
 		table.setAutoResizeMode( JTable.AUTO_RESIZE_OFF);
 		table.setFillsViewportHeight(true);	
 		TableColumn column = null;
-		for (int i = 0; i < games.size(); i++) {
+		for (int i = 0; i < games.size() + 1; i++) {
 		    column = table.getColumnModel().getColumn(i);
 		    column.setMinWidth(400); 
 		}
+		
+		DefaultTableCellRenderer center = new DefaultTableCellRenderer();
+		center.setHorizontalAlignment( JLabel.CENTER );
+		TableColumn column1 = table.getColumnModel().getColumn(0);
 		column1.setCellRenderer(center);
-		//column5.setCellRenderer(center);
 		table.setRowHeight(80);
+		
+		ViewGames.table.getTableHeader().repaint();
+		ViewGames.model.fireTableDataChanged();
+		
 		jf.getContentPane().add(scrollPane, BorderLayout.CENTER);
 		
 	}
