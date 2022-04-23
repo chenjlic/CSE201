a44f7bae35b5bc302cf61b7cfe714e9ea934d6be
soulitide <64114510+soulitide@users.noreply.github.com>
2022-04-22 20:08:59 -0400

    Add files via upload

---
+++ Filter.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.swing.RowFilter;
+import javax.swing.RowFilter.Entry;
+
+public class Filter {
+
+	/*
+	 * public class FilterByDeveloper extends RowFilter {
+	 * 
+	 * String dev; public FilterByDeveloper(String dev) { dev = dev; }
+	 * 
+	 * @Override public boolean include(Entry entry) {
+	 * 
+	 * if (dev.equals(entry.getStringValue(2))) return true; return false; }
+	 * 
+	 * }
+	 */
+	
+	public static ArrayList<VideoGame> filterByPrice (int low, int high, ArrayList<VideoGame> games) {
+
+		
+	games.removeIf(s -> !(s.getPrice() <= high && s.getPrice() >= low));
+	
+	return games;
+		
+	}
+	
+	public static ArrayList<VideoGame> filterByDev (String dev, ArrayList<VideoGame> games) {
+		games.removeIf(s -> !(s.getDeveloper().contains(dev)));
+		return games;
+	}
+	
+	
+	public static ArrayList<VideoGame> filterByPlatform(String platform, ArrayList<VideoGame> games) {
+		games.removeIf(s -> !(Arrays.toString(s.getPlatforms()).contains(platform)));
+		return games;
+		
+	}
+	
+
+	/*
+	 * public static ArrayList<VideoGame> filterByGenre (String genre,
+	 * ArrayList<VideoGame> games) { games.removeIf(s ->
+	 * !(s.getGenre().contains(genre)));
+	 * 
+	 * return games;
+	 * 
+	 * }
+	 */
+	
+
+}
